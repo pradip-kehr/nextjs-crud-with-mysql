b177ff70f4d20dde21984f4044fd1c8c9f110c04
Kanhasoft - 028 <pradeep@kanhasoft.com>
2024-05-24 11:41:04 +0530

    change database from mysql to postgres

---
+++ prisma/migrations/20240524060624_post/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
