90e0c048db9169f59497a5076f7b299206076300
Kanhasoft - 028 <pradeep@kanhasoft.com>
2024-05-31 17:59:37 +0530

    create emailsend demo

--- src/controller/emailsender.ts
+++ src/controller/emailsender.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import nodemailer from 'nodemailer';
+import handlebars from 'handlebars';
 export const sendEmail = async () => {
     let transporter = nodemailer.createTransport({
         service: 'gmail', // you can use any email service
@@ -9,13 +10,13 @@ export const sendEmail = async () => {
             pass: process.env.EMAIL_PASS, // your email password or application-specific password
         },
     });
+
+    const template = handlebars.compile(`htmlTemplate`);
+    const htmlToSend = template({ fileName: 'a' });
     await transporter.sendMail({
         from: process.env.EMAIL_USER, // sender address
         to: "pradeep@kanhasoft.com", // list of receivers
         subject: 'subject', // Subject line
-        text: "message", // plain text body 
-        // review code of Queue/worker and postgres with Prisma
-        // create demo for job and worker in next js project using instrumentation.ts file
-        // implement import excel functionality using Queue/worker
+        html: htmlToSend
     });
 }
